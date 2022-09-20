f2626ce3ff34bddc04c9943d89150bbb673c2b68
cesce <gules01@gmail.com>
2022-09-21 01:17:26 +0200

    Version 1.0.1

--- README.md
+++ README.md
@@ -6,8 +6,68 @@ This is an extension of the existing library: <https://github.com/uuidjs/uuid>
 
 ## Install
 
+```bash
+npm install uuidv6-extension
+```
+
+## API
+
+| Method             | Return                                                     | Comments                               |
+| ------------------ | ---------------------------------------------------------- | -------------------------------------- |
+| uuid.nil( )        | The nil UUID string (all zeros)                            | '00000000-0000-0000-0000-000000000000' |
+| uuid.parse( )      | Convert UUID string to array of bytes                      | ArrayLike<number>                      |
+| uuid.stringify( )  | Convert array of bytes to UUID string                      | '46ed45e0-392d-11ed-9370-0800200c9a66' |
+| uuid.validate( )   | Test a string to see if it is a valid UUID not v6          | Boolean                                |
+| uuid.validateV6( ) | Test a string to see if it is a valid UUID v6              | Boolean                                |
+| uuid.version( )    | Detect RFC version of a UUID not v6                        | Number                                 |
+| uuid.versionV6( )  | Detect RFC version of a UUID for v1 to v6                  | Number                                 |
+| uuid.uuidToDate( ) | Convert a UUID V1 to a Date Object                         | Date                                   |
+| uuid.v1( )         | Create a version 1 (timestamp) UUID                        | '5714f720-1268-11e7-a24b-96d95aa38c32' |
+| uuid.v4( )         | Create a version 4 (random) UUID                           | '81d86d15-822f-4f7b-b091-0a1f9392d379' |
+| uuid.v6( )         | Create a version 6 (ordered timestamp) UUID from version 1 | '1e712685-714f-6720-a23a-c90103f70be6' |
+
 ## Usage
 
-### Generate a new UUID v6 - Ordered time based
+```ts
+import * as uuid from "uuidv6-extension";
 
-### Get timestamp from UUID v6
+// nil
+console.log(`nil UUID: ${nil()}`);
+
+// Parse - Stringify
+const uuidV1 = v1();
+const uuidV1Parse = parse(uuidV1);
+const uuidV1Str = stringify(uuidV1Parse);
+const uuidV6 = v6();
+
+console.log(`UUID v1: ${uuidV1}`);
+console.log(`UUID v1 stringified: ${uuidV1Str}`);
+
+// Validate
+const invalid = "ad333a";
+console.log(validate(uuidV1));
+console.log(validate(invalid));
+console.log(validateV6(uuidV6));
+
+// Version
+console.log(version(uuidV1));
+console.log(versionV6(uuidV1));
+console.log(versionV6(uuidV6));
+
+// UUID to Date
+console.log(uuidToDate(uuidV1));
+```
+
+## To be done
+
+Functions:
+
+- uuidV6toV1()
+- uuidToDateV6()
+
+Test
+
+## License
+
+- This module is licensed under the [ISC License](https://github.com/kurttheviking/uuid-with-v6-js/blob/master/LICENSE).
+- The underlying uuid module is licensed under the [MIT License](https://github.com/kelektiv/node-uuid/blob/master/LICENSE.md).
--- lib/index.ts
+++ lib/index.ts
@@ -1,10 +1,110 @@
-import { v1 as uuidv1 } from "uuid";
+import * as crypto from "crypto";
+import * as uuid from "uuid";
 
-const UUIDVALIDATION =
-  "/^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i";
+// Return the nil UUID string (all zeros)
+export function nil(): string {
+  return uuid.NIL;
+}
 
-export function v6(v1: string): string {
-  const uuidv6 = uuidv1();
-  console.log(uuidv6);
-  return "123";
+// Convert UUID string to array of bytes
+export function parse(uuidData: string): ArrayLike<number> {
+  return uuid.parse(uuidData);
+}
+
+// Convert array of bytes to UUID string
+export function stringify(uuidData: ArrayLike<number>): string {
+  return uuid.stringify(uuidData);
+}
+
+// Test a string to see if it is a valid UUID not for v6
+export function validate(uuidData: string): boolean {
+  return uuid.validate(uuidData);
+}
+
+// Test a string to see if it is a valid UUID v6
+export function validateV6(uuiDate: string): boolean {
+  const regExpUuid = /^[a-z,0-9,-]{36,36}$/;
+
+  return regExpUuid.test(uuiDate);
+}
+
+// Detect RFC version of a UUID not for v6
+export function version(uuidData: string): number {
+  return uuid.version(uuidData);
+}
+
+// Detect RFC version of a UUID for v1 to v6
+export function versionV6(uuidData: string): number {
+  let version = 0;
+
+  if (validateV6(uuidData)) {
+    return parseInt(uuidData.charAt(14));
+  }
+
+  return uuid.version(uuidData);
+}
+
+// Convert a UUID V1 to a Date Object
+export function uuidToDate(uuidData: string): Date {
+  var GREGORIAN_OFFSET = 122192928000000000;
+
+  function get_time_int(uuid_str: string): number {
+    // (string) uuid_str format	=>		'11111111-2222-#333-4444-555555555555'
+    var uuid_arr = uuid_str.split("-"),
+      time_str = [uuid_arr[2].substring(1), uuid_arr[1], uuid_arr[0]].join("");
+    // time_str is convert  '11111111-2222-#333-4444-555555555555'  to  '333222211111111'
+    return parseInt(time_str, 16);
+  }
+
+  function get_date_obj(uuid_str: string): Date {
+    // (string) uuid_str format	=>		'11111111-2222-#333-4444-555555555555'
+    var int_time = get_time_int(uuid_str) - GREGORIAN_OFFSET,
+      int_millisec = Math.floor(int_time / 10000);
+    return new Date(int_millisec);
+  }
+
+  return get_date_obj(uuidData);
+}
+
+// Create a version 1 (timestamp) UUID
+export function v1(): string {
+  return uuid.v1();
+}
+
+// Create a version 4 (random) UUID
+export function v4(): string {
+  return uuid.v4();
+}
+
+// Create a version 6 (ordered timestamp) UUID from version 1
+export function v6(opts: any = {}): string {
+  const options = opts || {};
+
+  const disableRandom = Boolean(options.disableRandom);
+
+  function generateId(): string {
+    const raw = v1();
+
+    const prefix = `${raw.substring(15, 18)}${raw.substring(
+      9,
+      13
+    )}${raw.substring(0, 5)}6${raw.substring(5, 8)}`;
+
+    const prefixFormatted = `${prefix.substring(0, 8)}-${prefix.substring(
+      8,
+      4
+    )}-${prefix.substring(12)}`;
+
+    const random = crypto.randomBytes(8).toString("hex");
+
+    if (disableRandom) {
+      return `${prefixFormatted}${raw.substring(18)}`;
+    }
+
+    return `${prefixFormatted}-${random.substring(0, 4)}-${random.substring(
+      4
+    )}`;
+  }
+
+  return generateId();
 }
